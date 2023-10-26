cd72760287549fb300175995237ff6a3c8fdb111
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-26 16:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	const mathMethods = Object.getOwnPropertyNames(Math);
+  return mathMethods.join(', ');
 }
 
 alert(allMethods());
